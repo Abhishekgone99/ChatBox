d15849dbf247917d9b933fa22035a104080b6225
Abhishek Gone <abhishekgone99@gmail.com>
2024-07-10 10:05:20 +0530

    added some comments

--- index.js
+++ index.js
@@ -10,24 +10,32 @@ const server = http.createServer(app);
 const io = socket(server)
 
 
+// Serve the index.html file when accessing the root URL
 app.get('/',(req,res)=>{
     // res.sendFile('/index.html',{root:__dirname})
     res.sendFile(__dirname + '/index.html')
 })
 
+// Socket.IO connection handling
 io.on('connection',(socket)=>{
     console.log('New user connected');
+
+    // Listen for 'chat message' events from clients
     socket.on('chat message',(msg)=>{
         console.log('message:'+ msg);
+
+         // Broadcast the message to all connected clients
         io.emit('chat message', msg);
     })
 
+    // Handle disconnection of clients
     socket.on('disconnect',()=>{
         console.log('User disconnected');
     })
 })
 
 
+// Start the server and listen on port 3000
 server.listen(3000,()=>{
     console.log('listening to port 3000');
 })
